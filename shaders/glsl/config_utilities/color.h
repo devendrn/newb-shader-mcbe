e1225a210d2454a0e9102789324f606c8ddb0724
devendrn <devman2857@gmail.com>
2022-01-01 22:01:54 +0530

    Replaced Uncharted 2 TMO with ACES TMO

--- shaders/glsl/config_utilities/color.h
+++ shaders/glsl/config_utilities/color.h
@@ -8,7 +8,7 @@
 // 1 - Exponential
 // 2 - Simple Reinhard
 // 3 - Extended Reinhard (Default)
-// 4 - Uncharted 2
+// 4 - ACES
 #define TONEMAPPING_TYPE 3
 
 // Toggle + Value - Exposure
@@ -34,51 +34,40 @@
 
 #if TONEMAPPING_TYPE==3
 // extended reinhard tonemapping
-vec3 tonemap(vec3 color){
+vec3 tonemap(vec3 x){
 	//float white = 4.0;
 	//float white_scale = 1.0/(white*white);
 	float white_scale = 0.063;
 
-	color = (color*(1.0+(color*white_scale)))/(1.0+color);
+	x = (x*(1.0+(x*white_scale)))/(1.0+x);
 
-	//color = (color*1.32)/(1.0+color);
+	//x = (x*1.32)/(1.0+x);
 
-	return color;
+	return x;
 }
 
 #elif TONEMAPPING_TYPE==4
-// uncharted 2 tone mapping
-vec3 tonemap(vec3 color){
-	float A = 0.15;
-	float B = 0.50;
-	//float C = 0.10;
-	//float D = 0.20;
-	//float E = 0.02;
-	//float F = 0.30;
-
-	float exposure = 2.0;
-	vec3 x = color*exposure;
-
-	//color = ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F;
-	color = ((x*(A*x+0.05)+0.004)/(x*(A*x+B)+0.06))-0.06666667;
-
-	//float W = 11.2;
-	//float white_scale = 1.0/(((W*(A*W+C*B)+D*E)/(W*(A*W+B)+D*F))-E/F);
-	float white_scale = 1.37906425;
-
-	return color*white_scale;
+// aces tone mapping
+vec3 tonemap(vec3 x){
+	x *= 0.85;
+	const float a = 1.04;
+	const float b = 0.03;
+	const float c = 0.93;
+	const float d = 0.56;
+	const float e = 0.14;
+	return clamp((x * (a * x + b)) / (x * (c * x + d) + e), 0.0, 1.0);
 }
 
 #elif TONEMAPPING_TYPE==2
 // simple reinhard tonemapping
-vec3 tonemap(vec3 color){
-	return color/(1.0 + color);
+vec3 tonemap(vec3 x){
+	return x/(1.0 + x);
 }
 
 #elif TONEMAPPING_TYPE==1
 // exponential tonemapping
-vec3 tonemap(vec3 color){
-	return 1.0-exp(-color*0.8);
+vec3 tonemap(vec3 x){
+	return 1.0-exp(-x*0.8);
 }
 
 #endif
